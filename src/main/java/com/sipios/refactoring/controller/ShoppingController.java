26a5c9997c349896fd44fe69a5a7f207228424ba
Guy Verel <gverel@gmail.com>
2022-01-03 12:24:32 +0100

    Refacto ShoppingController : create methode computeTotalPrice + set total price as a double

--- src/main/java/com/sipios/refactoring/controller/ShoppingController.java
+++ src/main/java/com/sipios/refactoring/controller/ShoppingController.java
@@ -18,66 +18,7 @@ public class ShoppingController {
     public String getPrice(@RequestBody Body b) {
         double customerDiscount = computeCustomerDiscount(b.getCustomerType());
 
-        Date date = new Date();
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
-        cal.setTime(date);
-
-        // Compute total amount depending on the types and quantity of product and
-        // if we are in winter or summer discounts periods
-        double totalPrice = 0;
-        if (
-            !(
-                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                    cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                    cal.get(Calendar.MONTH) == Calendar.JUNE
-            ) &&
-                !(
-                    cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                        cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                        cal.get(Calendar.MONTH) == Calendar.JANUARY
-                )
-        ) {
-            if (b.getItems() == null) {
-                return "0";
-            }
-
-            for (int i = 0; i < b.getItems().length; i++) {
-                Item it = b.getItems()[i];
-
-                switch (it.getType()) {
-                    case "TSHIRT":
-                        totalPrice += 30 * it.getNb() * customerDiscount;
-                        break;
-                    case "DRESS":
-                        totalPrice += 50 * it.getNb() * customerDiscount;
-                        break;
-                    case "JACKET":
-                        totalPrice += 100 * it.getNb() * customerDiscount;
-                        break;
-                }
-            }
-        } else {
-            if (b.getItems() == null) {
-                return "0";
-            }
-
-            for (int i = 0; i < b.getItems().length; i++) {
-                Item it = b.getItems()[i];
-
-                switch (it.getType()) {
-                    case "TSHIRT":
-                        totalPrice += 30 * it.getNb() * customerDiscount;
-                        break;
-                    case "DRESS":
-                        totalPrice += 50 * it.getNb() * 0.8 * customerDiscount;
-                        break;
-                    case "JACKET":
-                        totalPrice += 100 * it.getNb() * 0.9 * customerDiscount;
-                        break;
-                }
-            }
-        }
-
+        double totalPrice = computeTotalPrice(b, customerDiscount);
         try {
             switch (b.getCustomerType()) {
                 case "STANDARD_CUSTOMER":
@@ -106,6 +47,69 @@ public class ShoppingController {
         }
 
         return String.valueOf(totalPrice);
+    }
+
+    double computeTotalPrice(Body b, double customerDiscount) {
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        cal.setTime(date);
+
+        // Compute total amount depending on the types and quantity of product and
+        // if we are in winter or summer discounts periods
+        double totalPrice = 0;
+        if (
+            !(
+                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+                    cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+                    cal.get(Calendar.MONTH) == Calendar.JUNE
+            ) &&
+                !(
+                    cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+                        cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+                        cal.get(Calendar.MONTH) == Calendar.JANUARY
+                )
+        ) {
+            if (b.getItems() == null) {
+                return 0;
+            }
+
+            for (int i = 0; i < b.getItems().length; i++) {
+                Item it = b.getItems()[i];
+
+                switch (it.getType()) {
+                    case "TSHIRT":
+                        totalPrice += 30 * it.getNb() * customerDiscount;
+                        break;
+                    case "DRESS":
+                        totalPrice += 50 * it.getNb() * customerDiscount;
+                        break;
+                    case "JACKET":
+                        totalPrice += 100 * it.getNb() * customerDiscount;
+                        break;
+                }
+            }
+        } else {
+            if (b.getItems() == null) {
+                return 0;
+            }
+
+            for (int i = 0; i < b.getItems().length; i++) {
+                Item it = b.getItems()[i];
+
+                switch (it.getType()) {
+                    case "TSHIRT":
+                        totalPrice += 30 * it.getNb() * customerDiscount;
+                        break;
+                    case "DRESS":
+                        totalPrice += 50 * it.getNb() * 0.8 * customerDiscount;
+                        break;
+                    case "JACKET":
+                        totalPrice += 100 * it.getNb() * 0.9 * customerDiscount;
+                        break;
+                }
+            }
+        }
+        return totalPrice;
     }
 
     double computeCustomerDiscount(String customerType) {
