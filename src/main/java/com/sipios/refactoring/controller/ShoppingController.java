51ac3f2a34db7b31ce62f8e954db4ed995c5cc7c
Guy Verel <gverel@gmail.com>
2022-01-03 12:17:55 +0100

    Refacto ShoppingController : renamin variables and create methode computeCustomerDiscount

--- src/main/java/com/sipios/refactoring/controller/ShoppingController.java
+++ src/main/java/com/sipios/refactoring/controller/ShoppingController.java
@@ -16,41 +16,26 @@ import java.util.TimeZone;
 public class ShoppingController {
     @PostMapping
     public String getPrice(@RequestBody Body b) {
-        double p = 0;
-        double d;
+        double customerDiscount = computeCustomerDiscount(b.getCustomerType());
 
         Date date = new Date();
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
         cal.setTime(date);
 
-        // Compute discount for customer
-        switch (b.getType()) {
-            case "STANDARD_CUSTOMER":
-                d = 1;
-                break;
-            case "PREMIUM_CUSTOMER":
-                d = 0.9;
-                break;
-            case "PLATINUM_CUSTOMER":
-                d = 0.5;
-                break;
-            default:
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
-
         // Compute total amount depending on the types and quantity of product and
         // if we are in winter or summer discounts periods
+        double totalPrice = 0;
         if (
             !(
                 cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == Calendar.JUNE
+                    cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+                    cal.get(Calendar.MONTH) == Calendar.JUNE
             ) &&
-            !(
-                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == Calendar.JANUARY
-            )
+                !(
+                    cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+                        cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+                        cal.get(Calendar.MONTH) == Calendar.JANUARY
+                )
         ) {
             if (b.getItems() == null) {
                 return "0";
@@ -61,13 +46,13 @@ public class ShoppingController {
 
                 switch (it.getType()) {
                     case "TSHIRT":
-                        p += 30 * it.getNb() * d;
+                        totalPrice += 30 * it.getNb() * customerDiscount;
                         break;
                     case "DRESS":
-                        p += 50 * it.getNb() * d;
+                        totalPrice += 50 * it.getNb() * customerDiscount;
                         break;
                     case "JACKET":
-                        p += 100 * it.getNb() * d;
+                        totalPrice += 100 * it.getNb() * customerDiscount;
                         break;
                 }
             }
@@ -81,38 +66,38 @@ public class ShoppingController {
 
                 switch (it.getType()) {
                     case "TSHIRT":
-                        p += 30 * it.getNb() * d;
+                        totalPrice += 30 * it.getNb() * customerDiscount;
                         break;
                     case "DRESS":
-                        p += 50 * it.getNb() * 0.8 * d;
+                        totalPrice += 50 * it.getNb() * 0.8 * customerDiscount;
                         break;
                     case "JACKET":
-                        p += 100 * it.getNb() * 0.9 * d;
+                        totalPrice += 100 * it.getNb() * 0.9 * customerDiscount;
                         break;
                 }
             }
         }
 
         try {
-            switch (b.getType()) {
+            switch (b.getCustomerType()) {
                 case "STANDARD_CUSTOMER":
-                    if (p > 200) {
-                        throw new Exception("Price (" + p + ") is too high for standard customer");
+                    if (totalPrice > 200) {
+                        throw new Exception("Price (" + totalPrice + ") is too high for standard customer");
                     }
                     break;
                 case "PREMIUM_CUSTOMER":
-                    if (p > 800) {
-                        throw new Exception("Price (" + p + ") is too high for premium customer");
+                    if (totalPrice > 800) {
+                        throw new Exception("Price (" + totalPrice + ") is too high for premium customer");
                     }
                     break;
                 case "PLATINUM_CUSTOMER":
-                    if (p > 2000) {
-                        throw new Exception("Price (" + p + ") is too high for platinum customer");
+                    if (totalPrice > 2000) {
+                        throw new Exception("Price (" + totalPrice + ") is too high for platinum customer");
                     }
                     break;
                 default:
-                    if (p > 200) {
-                        throw new Exception("Price (" + p + ") is too high for standard customer");
+                    if (totalPrice > 200) {
+                        throw new Exception("Price (" + totalPrice + ") is too high for standard customer");
                     }
                     break;
             }
@@ -120,26 +105,44 @@ public class ShoppingController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
         }
 
-        return String.valueOf(p);
+        return String.valueOf(totalPrice);
+    }
+
+    double computeCustomerDiscount(String customerType) {
+        final double customerDiscount;
+        switch (customerType) {
+            case "STANDARD_CUSTOMER":
+                customerDiscount = 1;
+                break;
+            case "PREMIUM_CUSTOMER":
+                customerDiscount = 0.9;
+                break;
+            case "PLATINUM_CUSTOMER":
+                customerDiscount = 0.5;
+                break;
+            default:
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        return customerDiscount;
     }
 }
 
 class Body {
 
     private final Item[] items;
-    private final String type;
+    private final String customerType;
 
     public Body(Item[] is, String t) {
         this.items = is;
-        this.type = t;
+        this.customerType = t;
     }
 
     public Item[] getItems() {
         return items;
     }
 
-    public String getType() {
-        return type;
+    public String getCustomerType() {
+        return customerType;
     }
 }
 
