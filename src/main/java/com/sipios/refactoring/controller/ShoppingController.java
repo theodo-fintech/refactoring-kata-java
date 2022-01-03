952d246e87787e6d653fb785056228f1c36c9c86
Guy Verel <gverel@gmail.com>
2022-01-03 12:02:19 +0100

    Refacto ShoppingController : remove some dead code + replace if else with swith

--- src/main/java/com/sipios/refactoring/controller/ShoppingController.java
+++ src/main/java/com/sipios/refactoring/controller/ShoppingController.java
@@ -1,10 +1,5 @@
 package com.sipios.refactoring.controller;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 @RestController
 @RequestMapping("/shopping")
 public class ShoppingController {
-
-    private Logger logger = LoggerFactory.getLogger(ShoppingController.class);
-
     @PostMapping
     public String getPrice(@RequestBody Body b) {
         double p = 0;
@@ -28,14 +24,18 @@ public class ShoppingController {
         cal.setTime(date);
 
         // Compute discount for customer
-        if (b.getType().equals("STANDARD_CUSTOMER")) {
-            d = 1;
-        } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
-            d = 0.9;
-        } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
-            d = 0.5;
-        } else {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        switch (b.getType()) {
+            case "STANDARD_CUSTOMER":
+                d = 1;
+                break;
+            case "PREMIUM_CUSTOMER":
+                d = 0.9;
+                break;
+            case "PLATINUM_CUSTOMER":
+                d = 0.5;
+                break;
+            default:
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
         // Compute total amount depending on the types and quantity of product and
@@ -44,12 +44,12 @@ public class ShoppingController {
             !(
                 cal.get(Calendar.DAY_OF_MONTH) < 15 &&
                 cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == 5
+                cal.get(Calendar.MONTH) == Calendar.JUNE
             ) &&
             !(
                 cal.get(Calendar.DAY_OF_MONTH) < 15 &&
                 cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == 0
+                cal.get(Calendar.MONTH) == Calendar.JANUARY
             )
         ) {
             if (b.getItems() == null) {
@@ -59,16 +59,17 @@ public class ShoppingController {
             for (int i = 0; i < b.getItems().length; i++) {
                 Item it = b.getItems()[i];
 
-                if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * d;
-                } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * d;
-                } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * d;
+                switch (it.getType()) {
+                    case "TSHIRT":
+                        p += 30 * it.getNb() * d;
+                        break;
+                    case "DRESS":
+                        p += 50 * it.getNb() * d;
+                        break;
+                    case "JACKET":
+                        p += 100 * it.getNb() * d;
+                        break;
                 }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
             }
         } else {
             if (b.getItems() == null) {
@@ -78,36 +79,42 @@ public class ShoppingController {
             for (int i = 0; i < b.getItems().length; i++) {
                 Item it = b.getItems()[i];
 
-                if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * d;
-                } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * 0.8 * d;
-                } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * 0.9 * d;
+                switch (it.getType()) {
+                    case "TSHIRT":
+                        p += 30 * it.getNb() * d;
+                        break;
+                    case "DRESS":
+                        p += 50 * it.getNb() * 0.8 * d;
+                        break;
+                    case "JACKET":
+                        p += 100 * it.getNb() * 0.9 * d;
+                        break;
                 }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
             }
         }
 
         try {
-            if (b.getType().equals("STANDARD_CUSTOMER")) {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
-                }
-            } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
-                if (p > 800) {
-                    throw new Exception("Price (" + p + ") is too high for premium customer");
-                }
-            } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
-                if (p > 2000) {
-                    throw new Exception("Price (" + p + ") is too high for platinum customer");
-                }
-            } else {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
-                }
+            switch (b.getType()) {
+                case "STANDARD_CUSTOMER":
+                    if (p > 200) {
+                        throw new Exception("Price (" + p + ") is too high for standard customer");
+                    }
+                    break;
+                case "PREMIUM_CUSTOMER":
+                    if (p > 800) {
+                        throw new Exception("Price (" + p + ") is too high for premium customer");
+                    }
+                    break;
+                case "PLATINUM_CUSTOMER":
+                    if (p > 2000) {
+                        throw new Exception("Price (" + p + ") is too high for platinum customer");
+                    }
+                    break;
+                default:
+                    if (p > 200) {
+                        throw new Exception("Price (" + p + ") is too high for standard customer");
+                    }
+                    break;
             }
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
@@ -119,39 +126,27 @@ public class ShoppingController {
 
 class Body {
 
-    private Item[] items;
-    private String type;
+    private final Item[] items;
+    private final String type;
 
     public Body(Item[] is, String t) {
         this.items = is;
         this.type = t;
     }
 
-    public Body() {}
-
     public Item[] getItems() {
         return items;
-    }
-
-    public void setItems(Item[] items) {
-        this.items = items;
     }
 
     public String getType() {
         return type;
     }
-
-    public void setType(String type) {
-        this.type = type;
-    }
 }
 
 class Item {
 
-    private String type;
-    private int nb;
-
-    public Item() {}
+    private final String type;
+    private final int nb;
 
     public Item(String type, int quantity) {
         this.type = type;
@@ -162,15 +157,8 @@ class Item {
         return type;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
-
     public int getNb() {
         return nb;
     }
 
-    public void setNb(int nb) {
-        this.nb = nb;
-    }
 }
