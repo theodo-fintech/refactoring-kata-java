92eada28cbe4823561869086d3280df9907b1fe1
Come Tisserand <cometisserand@rocketmail.com>
2022-01-05 09:52:26 +0100

    Refacto ShoppingController: moved logic to ShoppingService

--- src/main/java/com/sipios/refactoring/controller/ShoppingController.java
+++ src/main/java/com/sipios/refactoring/controller/ShoppingController.java
@@ -1,11 +1,8 @@
 package com.sipios.refactoring.controller;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 
 import com.sipios.refactoring.dto.ShoppingDetails;
-import com.sipios.refactoring.dto.ShoppingItem;
+import com.sipios.refactoring.service.ShoppingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -21,96 +18,38 @@ public class ShoppingController {
 
     private Logger logger = LoggerFactory.getLogger(ShoppingController.class);
 
+    private ShoppingService shoppingService = new ShoppingService();
+
     @PostMapping
     public String getPrice(@RequestBody ShoppingDetails s) {
-        double p = 0;
-        double d;
+        double p;
 
-        Date date = new Date();
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
-        cal.setTime(date);
-
-        // Compute discount for customer
-        if (s.getType().equals("STANDARD_CUSTOMER")) {
-            d = 1;
-        } else if (s.getType().equals("PREMIUM_CUSTOMER")) {
-            d = 0.9;
-        } else if (s.getType().equals("PLATINUM_CUSTOMER")) {
-            d = 0.5;
-        } else {
+        // Calculate total shopping price
+        try {
+            p = shoppingService.getPrice(s);
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        // Compute total amount depending on the types and quantity of product and
-        // if we are in winter or summer discounts periods
-        if (
-            !(
-                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == 5
-            ) &&
-            !(
-                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == 0
-            )
-        ) {
-            if (s.getItems() == null) {
-                return "0";
-            }
-
-            for (int i = 0; i < s.getItems().length; i++) {
-                ShoppingItem it = s.getItems()[i];
-
-                if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * d;
-                } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * d;
-                } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * d;
-                }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
-            }
-        } else {
-            if (s.getItems() == null) {
-                return "0";
-            }
-
-            for (int i = 0; i < s.getItems().length; i++) {
-                ShoppingItem it = s.getItems()[i];
-
-                if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * d;
-                } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * 0.8 * d;
-                } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * 0.9 * d;
-                }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
-            }
-        }
-
+        // Check if shopping price is too high for customer type
         try {
-            if (s.getType().equals("STANDARD_CUSTOMER")) {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
-                }
-            } else if (s.getType().equals("PREMIUM_CUSTOMER")) {
-                if (p > 800) {
-                    throw new Exception("Price (" + p + ") is too high for premium customer");
-                }
-            } else if (s.getType().equals("PLATINUM_CUSTOMER")) {
-                if (p > 2000) {
-                    throw new Exception("Price (" + p + ") is too high for platinum customer");
-                }
-            } else {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
-                }
+            switch (s.getType()) {
+                case "STANDARD_CUSTOMER":
+                    if (p > 200) {
+                        throw new Exception("Price (" + p + ") is too high for standard customer");
+                    }
+                case "PREMIUM_CUSTOMER":
+                    if (p > 800) {
+                        throw new Exception("Price (" + p + ") is too high for premium customer");
+                    }
+                case "PLATINUM_CUSTOMER":
+                    if (p > 2000) {
+                        throw new Exception("Price (" + p + ") is too high for platinum customer");
+                    }
+                default:
+                    if (p > 200) {
+                        throw new Exception("Price (" + p + ") is too high for standard customer");
+                    }
             }
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
---
+++ src/main/java/com/sipios/refactoring/service/ShoppingService.java
@@ -0,0 +1,104 @@
+package com.sipios.refactoring.service;
+
+import com.sipios.refactoring.dto.ShoppingDetails;
+import com.sipios.refactoring.dto.ShoppingItem;
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+@Component
+public class ShoppingService {
+
+    public double getPrice (ShoppingDetails s) throws Exception {
+        double priceShopping = 0;
+        double customerDiscount;
+
+        if (s.getItems() == null) {
+            return priceShopping;
+        }
+
+        // Compute discount for customer
+        customerDiscount = getDiscountOnCustomerType(s.getType());
+
+        // Compute total amount depending on the types and quantity of product and
+        // if we are in winter or summer discounts periods
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        cal.setTime(date);
+
+        for (int i = 0; i < s.getItems().length; i++) {
+            ShoppingItem it = s.getItems()[i];
+            try {
+                priceShopping += getItemBasePrice(it)
+                    * it.getNb()
+                    * getItemDiscount(it, cal)
+                    * customerDiscount;
+            } catch (Exception e) {
+                // TODO: handle invalid item
+            }
+        }
+        return priceShopping;
+    }
+
+    private double getDiscountOnCustomerType(String type) throws Exception {
+        switch (type) {
+            case "STANDARD_CUSTOMER":
+                return 1;
+            case "PREMIUM_CUSTOMER":
+                return 0.9;
+            case "PLATINUM_CUSTOMER":
+                return 0.5;
+            default:
+                throw new Exception("Invalid Customer type");
+        }
+    }
+
+    private boolean isSummerDiscountPeriod(Calendar cal) {
+        return cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+            cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+            cal.get(Calendar.MONTH) == Calendar.JUNE;
+    }
+
+    private boolean isWinterDiscountPeriod(Calendar cal) {
+        return cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+            cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+            cal.get(Calendar.MONTH) == Calendar.JANUARY;
+    }
+
+    private double getItemDiscount(ShoppingItem it, Calendar cal) throws Exception {
+        // Check if its Summer / Winter discount period
+        if (!isSummerDiscountPeriod(cal) && !isWinterDiscountPeriod(cal)) {
+            return 1;
+        }
+
+        switch (it.getType()) {
+            case "TSHIRT":
+                return 1;
+            case "DRESS":
+                return 0.8;
+            case "JACKET":
+                return 0.9;
+            // case "SWEATSHIRT":
+            //
+            default:
+                throw new Exception("Invalid shopping item");
+        }
+    }
+
+    private int getItemBasePrice(ShoppingItem it) throws Exception {
+        switch (it.getType()) {
+            case "TSHIRT":
+                return 30;
+            case "DRESS":
+                return 50;
+            case "JACKET":
+                return 100;
+            // case "SWEATSHIRT":
+            //      return 80;
+            default:
+                throw new Exception("Invalid shopping item");
+        }
+    }
+}
