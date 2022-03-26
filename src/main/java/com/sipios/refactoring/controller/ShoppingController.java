9e2afc5113bd75338d294703ad08933102035fb2
Xavier Melendez <reivax.melendez@gmail.com>
2022-03-26 12:27:40 +0100

    Refactoring interne au controlleur, suppression de duplication de code

--- src/main/java/com/sipios/refactoring/controller/ShoppingController.java
+++ src/main/java/com/sipios/refactoring/controller/ShoppingController.java
@@ -1,8 +1,9 @@
 package com.sipios.refactoring.controller;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -18,102 +19,92 @@ public class ShoppingController {
 
     private Logger logger = LoggerFactory.getLogger(ShoppingController.class);
 
+    private static final Map<String,Double> customerPlanDiscounts = Stream.of(new Object[][] {
+        { "STANDARD_CUSTOMER", 1d },
+        { "PREMIUM_CUSTOMER", 0.9d },
+        { "PLATINUM_CUSTOMER", 0.5d }
+    }).collect(Collectors.toMap(data -> (String) data[0], data -> (Double) data[1]));
+
+    private static final Map<String,Double> lowSeasonDiscounts = Stream.of(new Object[][] {
+        { "TSHIRT", 1d },
+        { "DRESS", 0.9d },
+        { "JACKET", 0.5d }
+    }).collect(Collectors.toMap(data -> (String) data[0], data -> (Double) data[1]));
+
+
+    private static final Map<String,Double> itemPriceList = Stream.of(new Object[][] {
+        { "TSHIRT", 30d },
+        { "DRESS", 50d },
+        { "JACKET", 100d }
+    }).collect(Collectors.toMap(data -> (String) data[0], data -> (Double) data[1]));
+
+    private static final Map<String,String> logCustomerType = Stream.of(new Object[][] {
+        { "STANDARD_CUSTOMER", "standard" },
+        { "PREMIUM_CUSTOMER", "premium" },
+        { "PLATINUM_CUSTOMER", "platinium" }
+    }).collect(Collectors.toMap(data -> (String) data[0], data -> (String) data[1]));
+
+    private static final Map<String,Double> customerLimitPrice = Stream.of(new Object[][] {
+        { "STANDARD_CUSTOMER", 200d },
+        { "PREMIUM_CUSTOMER", 800d },
+        { "PLATINUM_CUSTOMER", 2000d }
+    }).collect(Collectors.toMap(data -> (String) data[0], data -> (Double) data[1]));
+
+
+    private double computeDiscount(String type){
+        if ( !customerPlanDiscounts.containsKey(type)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }else {
+            return customerPlanDiscounts.get(type).doubleValue();
+        }
+    }
+
+    private boolean isSeasonalDiscountPeriod(Calendar cal){
+        return !(
+        cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+        cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+        cal.get(Calendar.MONTH) == Calendar.JUNE
+            ) &&
+                !(
+                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+        cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+        cal.get(Calendar.MONTH) == Calendar.JANUARY
+            ) ;
+    }
     @PostMapping
     public String getPrice(@RequestBody Body b) {
-        double p = 0;
-        double d;
+        if (b.getItems() == null || b.getItems().length == 0) {
+            return "0";
+        }
+        double totalPrice = 0;
+        double discount;
 
         Date date = new Date();
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
         cal.setTime(date);
 
-        // Compute discount for customer
-        if (b.getType().equals("STANDARD_CUSTOMER")) {
-            d = 1;
-        } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
-            d = 0.9;
-        } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
-            d = 0.5;
-        } else {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
+        discount = computeDiscount(b.getType());
 
         // Compute total amount depending on the types and quantity of product and
         // if we are in winter or summer discounts periods
-        if (
-            !(
-                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == 5
-            ) &&
-            !(
-                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
-                cal.get(Calendar.DAY_OF_MONTH) > 5 &&
-                cal.get(Calendar.MONTH) == 0
-            )
-        ) {
-            if (b.getItems() == null) {
-                return "0";
-            }
+        if (isSeasonalDiscountPeriod(cal)) {
 
-            for (int i = 0; i < b.getItems().length; i++) {
-                Item it = b.getItems()[i];
 
-                if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * d;
-                } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * d;
-                } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * d;
-                }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
-            }
+            totalPrice = Arrays.stream(b.getItems())
+                .mapToDouble(item -> itemPriceList.getOrDefault(item.getType(), 0d) * item.getNb() * discount)
+                .sum();
+
         } else {
-            if (b.getItems() == null) {
-                return "0";
-            }
-
-            for (int i = 0; i < b.getItems().length; i++) {
-                Item it = b.getItems()[i];
-
-                if (it.getType().equals("TSHIRT")) {
-                    p += 30 * it.getNb() * d;
-                } else if (it.getType().equals("DRESS")) {
-                    p += 50 * it.getNb() * 0.8 * d;
-                } else if (it.getType().equals("JACKET")) {
-                    p += 100 * it.getNb() * 0.9 * d;
-                }
-                // else if (it.getType().equals("SWEATSHIRT")) {
-                //     price += 80 * it.getNb();
-                // }
-            }
+            totalPrice = Arrays.stream(b.getItems())
+                .mapToDouble(item -> itemPriceList.getOrDefault(item.getType(), 0d) * item.getNb() * discount * lowSeasonDiscounts.getOrDefault(item.getType(),1d))
+                .sum();
         }
 
-        try {
-            if (b.getType().equals("STANDARD_CUSTOMER")) {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
-                }
-            } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
-                if (p > 800) {
-                    throw new Exception("Price (" + p + ") is too high for premium customer");
-                }
-            } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
-                if (p > 2000) {
-                    throw new Exception("Price (" + p + ") is too high for platinum customer");
-                }
-            } else {
-                if (p > 200) {
-                    throw new Exception("Price (" + p + ") is too high for standard customer");
-                }
-            }
-        } catch (Exception e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        if (totalPrice > customerLimitPrice.getOrDefault(b.getType(),customerLimitPrice.get("STANDARD_CUSTOMER"))){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Price (" + totalPrice + ") is too high for " + logCustomerType.getOrDefault(b.getType(),logCustomerType.get("STANDARD_CUSTOMER")));
         }
 
-        return String.valueOf(p);
+        return String.valueOf(totalPrice);
     }
 }
 
