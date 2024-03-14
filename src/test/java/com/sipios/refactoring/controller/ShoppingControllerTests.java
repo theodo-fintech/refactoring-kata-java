3c326c97cdabf8ff8d414cce93660f3f90a7eb16
ismail.daoudy-extern <ismail.daoudy-extern@geopost.com>
2024-03-14 20:39:46 +0100

    [REFACTO] Cover Test #4: Compute total price per customer and item type

--- src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -3,7 +3,12 @@ package com.sipios.refactoring.controller;
 import com.sipios.refactoring.UnitTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -45,5 +50,50 @@ class ShoppingControllerTests extends UnitTest {
         var body = new Body(items, "STANDARD_CUSTOMER");
         var price = controller.getPrice(body);
         assertThat(price).isEqualTo("0.0");
+    }
+
+    public static Stream<Arguments> totalPriceCalculationPerItemAndPerCustomerTypeTestCases() {
+        return Stream.of(
+            // Regular period
+            Arguments.of("STANDARD_CUSTOMER", "TSHIRT", 3, "90.0", false),
+            Arguments.of("STANDARD_CUSTOMER", "DRESS", 1, "50.0", false),
+            Arguments.of("STANDARD_CUSTOMER", "JACKET", 2, "200.0", false),
+
+            Arguments.of("PREMIUM_CUSTOMER", "TSHIRT", 3, "81.0", false),
+            Arguments.of("PREMIUM_CUSTOMER", "DRESS", 1, "45.0", false),
+            Arguments.of("PREMIUM_CUSTOMER", "JACKET", 2, "180.0", false),
+
+            Arguments.of("PLATINUM_CUSTOMER", "TSHIRT", 3, "45.0", false),
+            Arguments.of("PLATINUM_CUSTOMER", "DRESS", 1, "25.0", false),
+            Arguments.of("PLATINUM_CUSTOMER", "JACKET", 2, "100.0", false),
+
+
+            // Discount period
+            Arguments.of("STANDARD_CUSTOMER", "TSHIRT", 3, "90.0", true),
+            Arguments.of("STANDARD_CUSTOMER", "DRESS", 1, "40.0", true),
+            Arguments.of("STANDARD_CUSTOMER", "JACKET", 2, "180.0", true),
+
+            Arguments.of("PREMIUM_CUSTOMER", "TSHIRT", 3, "81.0", true),
+            Arguments.of("PREMIUM_CUSTOMER", "DRESS", 1, "36.0", true),
+            Arguments.of("PREMIUM_CUSTOMER", "JACKET", 2, "162.0", true),
+
+            Arguments.of("PLATINUM_CUSTOMER", "TSHIRT", 3, "45.0", true),
+            Arguments.of("PLATINUM_CUSTOMER", "DRESS", 1, "20.0", true),
+            Arguments.of("PLATINUM_CUSTOMER", "JACKET", 2, "90.0", true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("totalPriceCalculationPerItemAndPerCustomerTypeTestCases")
+    void should_calculate_total_price_per_item_type_per_customer_type(String customerType,
+                                                                      String itemType,
+                                                                      int quantity,
+                                                                      String expectedPrice,
+                                                                      boolean isDiscountPeriod) {
+        var testableController = new TestableShoppingController(isDiscountPeriod);
+        var items = new Item[]{new Item(itemType, quantity)};
+        var body = new Body(items, customerType);
+        var price = testableController.getPrice(body);
+        assertThat(price).isEqualTo(expectedPrice);
     }
 }
