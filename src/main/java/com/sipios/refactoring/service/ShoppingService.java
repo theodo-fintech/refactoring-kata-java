a4a6b119fbf16c215b3a2c816540aa4ec51c780a
Othmane Radouani <othmaneradouani@gmail.com>
2024-04-21 22:59:11 +0200

    create getPrice service method

--- src/main/java/com/sipios/refactoring/service/ShoppingService.java
+++ src/main/java/com/sipios/refactoring/service/ShoppingService.java
@@ -1,8 +1,103 @@
 package com.sipios.refactoring.service;
 
+import com.sipios.refactoring.models.Body;
+import com.sipios.refactoring.models.Item;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 @Service
 public class ShoppingService {
 
+
+    public String getPrice( Body b) throws Exception {
+        double p = 0;
+        double d;
+
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Europe/Paris"));
+        cal.setTime(date);
+
+        // Compute discount for customer
+        if (b.getType().equals("STANDARD_CUSTOMER")) {
+            d = 1;
+        } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
+            d = 0.9;
+        } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
+            d = 0.5;
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // Compute total amount depending on the types and quantity of product and
+        // if we are in winter or summer discounts periods
+        if (
+            !(
+                cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+                    cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+                    cal.get(Calendar.MONTH) == 5
+            ) &&
+                !(
+                    cal.get(Calendar.DAY_OF_MONTH) < 15 &&
+                        cal.get(Calendar.DAY_OF_MONTH) > 5 &&
+                        cal.get(Calendar.MONTH) == 0
+                )
+        ) {
+            if (b.getItems() == null) {
+                return "0";
+            }
+
+            for (int i = 0; i < b.getItems().length; i++) {
+                Item it = b.getItems()[i];
+
+                if (it.getType().equals("TSHIRT")) {
+                    p += 30 * it.getNb() * d;
+                } else if (it.getType().equals("DRESS")) {
+                    p += 50 * it.getNb() * d;
+                } else if (it.getType().equals("JACKET")) {
+                    p += 100 * it.getNb() * d;
+                }
+            }
+        } else {
+            if (b.getItems() == null) {
+                return "0";
+            }
+
+            for (int i = 0; i < b.getItems().length; i++) {
+                Item it = b.getItems()[i];
+
+                if (it.getType().equals("TSHIRT")) {
+                    p += 30 * it.getNb() * d;
+                } else if (it.getType().equals("DRESS")) {
+                    p += 50 * it.getNb() * 0.8 * d;
+                } else if (it.getType().equals("JACKET")) {
+                    p += 100 * it.getNb() * 0.9 * d;
+                }
+            }
+        }
+
+        if (b.getType().equals("STANDARD_CUSTOMER")) {
+            if (p > 200) {
+                throw new Exception("Price (" + p + ") is too high for standard customer");
+            }
+        } else if (b.getType().equals("PREMIUM_CUSTOMER")) {
+            if (p > 800) {
+                throw new Exception("Price (" + p + ") is too high for premium customer");
+            }
+        } else if (b.getType().equals("PLATINUM_CUSTOMER")) {
+            if (p > 2000) {
+                throw new Exception("Price (" + p + ") is too high for platinum customer");
+            }
+        } else {
+            if (p > 200) {
+                throw new Exception("Price (" + p + ") is too high for standard customer");
+            }
+        }
+
+        return String.valueOf(p);
+    }
 }
